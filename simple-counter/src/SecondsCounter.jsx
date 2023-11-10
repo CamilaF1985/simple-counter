d9d853c8b4c214e393aeec9ed368e0bc7ab05e12
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-11-10 17:24:45 -0300

    input para la cuenta regresiva

--- simple-counter/src/SecondsCounter.jsx
+++ simple-counter/src/SecondsCounter.jsx
@@ -2,99 +2,110 @@ import React, { useState, useEffect } from 'react';
 import StartButton from './StartButton';
 import StopButton from './StopButton';
 import ResetButton from './ResetButton';
-import Alert from './Alert';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function SecondsCounter() {
+  // Estado para manejar los segundos del contador
   const [segundos, setSegundos] = useState(0);
-  const [contandoHaciaArriba, setContandoHaciaArriba] = useState(true);
-  const [cuentaRegresiva, setCuentaRegresiva] = useState(false);
-  const [pausado, setPausado] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [inputValue, setInputValue] = useState('');
-  const [tiempoObjetivo, setTiempoObjetivo] = useState(0);
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+  // Estado para manejar si se está contando hacia arriba o hacia abajo
+  const [contandoHaciaArriba, setContandoHaciaArriba] = useState(true);
+
+  // Estado para manejar si el contador está pausado o no
+  const [pausado, setPausado] = useState(false);
+
+  // Estado para manejar el valor del input de la cuenta regresiva
+  const [inputValueCountdown, setInputValueCountdown] = useState('');
+
+  // Estado para manejar el valor del input de la alerta
+  const [inputValueAlert, setInputValueAlert] = useState('');
+
+  // Estado para el tiempo objetivo de la cuenta regresiva
+  const [tiempoObjetivoCountdown, setTiempoObjetivoCountdown] = useState(0);
+
+  // Estado para el tiempo objetivo de la alerta
+  const [tiempoObjetivoAlert, setTiempoObjetivoAlert] = useState(0);
+
+  // Maneja el cambio en el input de la cuenta regresiva
+  const handleInputCountdownChange = (e) => {
+    setInputValueCountdown(e.target.value);
   };
 
+  // Maneja el cambio en el input de la alerta
+  const handleInputAlertChange = (e) => {
+    setInputValueAlert(e.target.value);
+  };
+
+  // Inicia el contador
   const handleStart = () => {
     setPausado(false);
-
-    if (cuentaRegresiva) {
-      setContandoHaciaArriba(false);
-    } else {
-      setContandoHaciaArriba(true);
-    }
   };
 
+  // Detiene el contador
   const handleStop = () => {
     setPausado(true);
   };
 
+  // Reinicia el contador
   const handleReset = () => {
     setPausado(false);
     setSegundos(0);
-    setContandoHaciaArriba(!cuentaRegresiva);
-    setCuentaRegresiva(false);
+    setContandoHaciaArriba(true);
   };
 
-  const handleCloseAlert = () => {
-    setShowAlert(false);
-  };
-
-  const handleSetAlert = () => {
-    const segundosInput = parseInt(inputValue, 10);
+  // Establece la cuenta regresiva
+  const handleSetCountdown = () => {
+    const segundosInput = parseInt(inputValueCountdown, 10);
     if (!isNaN(segundosInput)) {
-      setTiempoObjetivo(segundosInput);
-      setShowAlert(false);
+      setContandoHaciaArriba(true);
+      setPausado(false);
+      setTiempoObjetivoCountdown(segundosInput);
+      toast.success('Cuenta regresiva establecida');
     }
   };
 
-  useEffect(() => {
-    if (segundos === tiempoObjetivo && tiempoObjetivo !== 0) {
-      setShowAlert(true);
+  // Establece la alerta
+  const handleSetAlert = () => {
+    const segundosInput = parseInt(inputValueAlert, 10);
+    if (!isNaN(segundosInput)) {
+      setTiempoObjetivoAlert(segundosInput);
+      toast.success('Alerta establecida');
     }
-  }, [segundos, tiempoObjetivo]);
+  };
 
+  // Efecto para manejar la lógica del contador
   useEffect(() => {
     let intervalId;
 
     if (!pausado) {
-      if (contandoHaciaArriba) {
-        intervalId = setInterval(() => {
-          if (segundos < 60) {
-            setSegundos(segundos + 1);
-          } else {
-            setContandoHaciaArriba(false);
-            setCuentaRegresiva(true);
-          }
-        }, 1000);
-      } else if (cuentaRegresiva) {
-        intervalId = setInterval(() => {
-          if (segundos > 0) {
-            setSegundos(segundos - 1);
-          } else {
-            setContandoHaciaArriba(true);
-            setCuentaRegresiva(false);
-          }
-        }, 1000);
-      }
+      intervalId = setInterval(() => {
+        if (contandoHaciaArriba) {
+          setSegundos((prevSegundos) => prevSegundos + 1);
+        } else if (segundos > 0) {
+          setSegundos((prevSegundos) => prevSegundos - 1);
+        }
+      }, 1000);
     }
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [segundos, contandoHaciaArriba, cuentaRegresiva, pausado]);
+  }, [segundos, contandoHaciaArriba, pausado, tiempoObjetivoCountdown]);
 
-  const maxDigitCount = 6;
+  // Efecto para mostrar mensajes de éxito
+  useEffect(() => {
+    if (!pausado && contandoHaciaArriba && segundos === tiempoObjetivoCountdown && tiempoObjetivoCountdown !== 0) {
+      setContandoHaciaArriba(false);
+      toast.success('Inicia la cuenta regresiva...');
+    }
 
-  const formatDigits = (digit) => {
-    return digit.toString().padStart(maxDigitCount, '0').slice(-maxDigitCount);
-  };
+    if (segundos === tiempoObjetivoAlert && tiempoObjetivoAlert !== 0) {
+      toast.success('Alerta activada');
+    }
+  }, [segundos, tiempoObjetivoCountdown, pausado, tiempoObjetivoAlert, contandoHaciaArriba]);
 
-  const secondsString = formatDigits(segundos);
-
+  // Renderiza el componente
   return (
     <div className="contador-container">
       <div className="contador">
@@ -102,7 +113,8 @@ function SecondsCounter() {
           <i className="far fa-clock"></i>
         </div>
         <div className="digitos">
-          {secondsString.split('').map((digit, index) => (
+          {/* Muestra los dígitos del contador */}
+          {segundos.toString().padStart(6, '0').split('').map((digit, index) => (
             <div className="digito" key={index}>
               {digit}
             </div>
@@ -111,26 +123,75 @@ function SecondsCounter() {
       </div>
       <div className="botones-container">
         <div className="botones">
+          {/* Botones para iniciar, detener y reiniciar el contador */}
           <StartButton onStart={handleStart} />
           <StopButton onStop={handleStop} />
           <ResetButton onReset={handleReset} />
         </div>
         <div className="input-container">
+          {/* Input y botón para establecer la cuenta regresiva */}
           <input
             type="number"
-            placeholder="Ingresa segundos"
-            value={inputValue}
-            onChange={handleInputChange}
+            placeholder="Ingresa segundos cuenta regresiva"
+            value={inputValueCountdown}
+            onChange={handleInputCountdownChange}
+          />
+          <button onClick={handleSetCountdown}>Establecer Cuenta Regresiva</button>
+        </div>
+        <div className="input-container">
+          {/* Input y botón para establecer la alerta */}
+          <input
+            type="number"
+            placeholder="Ingresa segundos para la alerta"
+            value={inputValueAlert}
+            onChange={handleInputAlertChange}
           />
           <button onClick={handleSetAlert}>Establecer Alerta</button>
         </div>
       </div>
-      <Alert showAlert={showAlert} onClose={handleCloseAlert} />
+      <ToastContainer />
     </div>
   );
 }
 
 export default SecondsCounter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
